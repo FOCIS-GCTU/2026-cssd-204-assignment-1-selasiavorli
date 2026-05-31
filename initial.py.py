5c0f7099d88cc3ee7921d34d822eafb3af9d9e90
selasiavorli <2425404545@live.gctu.edu.gh>
2026-05-31 19:35:21 +0000

    Add files via upload

---
+++ initial.py.py
@@ -0,0 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Selasi Avorli
+# STUDENT ID:  2425404545
+# Email: selasiavorli0@gmail.com
+# Grader: <YOUR GRADER'S NAME Carolyn OR Emma or Ahmad>
+#
+# On my honor, Selasi Avorli, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials SAV in large block letters (12 wide x 10 high),
+    # separated by three periods, with a large asterisk period at the end.
+
+    print()
+    print("...SAV")
+    print()
+    print("...SSSSSSSSSSSS...    AAAA    ...VV        VV...")
+    print("...SS          ...   AA  AA   ...VV        VV...")
+    print("...SS          ...  AA    AA  ... VV      VV ...")
+    print("...SSSSSSSSSSSS... AA      AA ... VV      VV ...")
+    print("...          SS...AAAAAAAAAAAA...  VV    VV  ...")
+    print("...          SS...AA        AA...   VV  VV   ...")
+    print("...SSSSSSSSSSSS...AA        AA...    VVVV    ...")
+    print("...            ...            ...            ...")
+    print("...            ...            ...            ...**")
+    print("...            ...            ...            ...**")
+    print()
+
+
+main()
